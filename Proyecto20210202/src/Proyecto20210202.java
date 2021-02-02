dacfb6b6dce03d61c05cea215271b7a0eaf081e0
rosillo <rosillo_xav@gva.es>
2021-02-02 19:22:14 +0100

    4 commit

--- Proyecto20210202/src/Proyecto20210202.java
+++ Proyecto20210202/src/Proyecto20210202.java
@@ -14,6 +14,9 @@ public class Proyecto20210202 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer");
+		System.out.println("Primer");
+		System.out.println("Primer");
+		System.out.println("Primer");
 
 	}
 
